5a7b7f98eb5950cadde90efda82f2981065adb28
kamly77 <bakoko2326@mail.ru>
2022-10-12 14:31:17 +0300

    Практическая 2

---
+++ Program.cs
@@ -0,0 +1,102 @@
+﻿//Random rand = new Random();
+//int MyNum;
+//int secret = rand.Next(1, 101);
+//int count = 1;
+//Console.WriteLine("Компьютер загадал число от 0 до 100.Попробуйте отгадать");
+//while (true)
+//{
+//    MyNum = int.Parse(Console.ReadLine());
+//    if (MyNum == secret)
+//    {
+//        Console.WriteLine("Угадал,бро");
+//        break;
+//    }
+//    else
+//    {
+//        if (MyNum > secret)
+//        {
+//            Console.WriteLine("Меньше,бро");
+//        }
+//        else
+//        {
+//            Console.WriteLine("Больше,бро");
+//        }
+//    }
+//}
+
+//Console.ReadKey();
+
+
+
+
+//public static void multiplytable()
+
+//{
+//    Console.WriteLine("█░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█");
+//    Console.WriteLine("█░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█");
+//    Console.WriteLine("█░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█");
+//    Console.WriteLine("█░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█");
+//    Console.WriteLine("█░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█");
+//    Console.WriteLine("█░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█");
+//    Console.WriteLine("█░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█");
+//    Console.WriteLine("█░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█");
+//    Console.WriteLine("█░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█");
+//    Console.WriteLine("█░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█");
+//    Console.WriteLine("█░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█");
+//    Console.WriteLine("█░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█");
+//    Console.WriteLine("█░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█");
+//    Console.WriteLine("█░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█");
+//    Console.WriteLine("█░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█");
+//    Console.WriteLine("█░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█");
+//    Console.WriteLine("█░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█");
+//    Console.WriteLine("█░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█");
+//    Console.WriteLine("█░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█");
+//    Console.WriteLine("█░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█");
+//    Console.WriteLine("█████████████████████████████████████████████████████████████████████████████████████");
+//    int cursorheight = 25;
+//    int cursorwidth = 20;
+//    Console.SetCursorPosition(cursorwidth, cursorheight);
+//    int[,] multiplytable = new int[10, 10];
+//    for (int i = 1; i <= multiplytable.GetLength(0); i++)
+//    {
+//        Console.SetCursorPosition(cursorwidth, cursorheight);
+//        for (int a = 1; a <= multiplytable.GetLength(1); a++)
+//        {
+//            multiplytable[i - 1, a - 1] = i * a;
+
+//            Console.Write($"{multiplytable[i - 1, a - 1],3} ");
+//            Thread.Sleep(20);
+//        }
+//        cursorheight += 2;
+//        Console.WriteLine();
+
+//    }
+//    string logoauthor = "press any button to exit to menu...";
+//    for (int i = 0; i < 36; i++)
+//    {
+//        Console.Write(logoauthor[i]);
+//        Thread.Sleep(30);
+//    }
+//    Thread.Sleep(30);
+//    Console.ReadKey();
+//    Console.Clear();
+//    faststart();
+//    Programs();
+//}
+
+
+//using System;
+
+//public class Test
+//{
+//    public static void Main()
+//    {
+//        Console.Write("Введите число ");
+//        int num = int.Parse(Console.ReadLine());
+//        for (int i = 1; i <= num; i++)
+//            if (num % i == 0)
+//                Console.WriteLine("Число {0} делится без остатка на {1}", num, i);
+
+//        Console.ReadKey();
+//    }
+//}
